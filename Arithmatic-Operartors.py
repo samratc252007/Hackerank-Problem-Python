bce7fc8998cb5402aa09d31c6856b474f180268a
Samrat Choudhury <samrat252007@gmail.com>
2026-02-10 23:23:41 +0530

    Arithmatic-Operartors.py

---
+++ Arithmatic-Operartors.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Print the sum of the two numbers
+    print(a + b)
+    # Print the difference of the two numbers
+    print(a - b)
+    # Print the product of the two numbers
+    print(a * b)
